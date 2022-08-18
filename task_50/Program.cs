18c64df798d56fe9af4847e9a334d5d53d7aa59f
Vyacheslav A <anvwcode@gmail.com>
2022-08-19 00:58:15 +0300

    47, 50, 52 completed

---
+++ task_50/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задача 50.
+// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1, 7 -> такого числа в массиве нет
+
+Console.Clear();
+Console.Write("Укажите позицию элемента в строке массива: ");
+int ElRowPos = Convert.ToInt32(Console.ReadLine());
+Console.Write("Укажите позицию элемента в столбце массива: ");
+int ElColPos = Convert.ToInt32(Console.ReadLine());
+int[,] matrixResult = GetMatrix(3, 4);
+
+PrintMatrix(matrixResult);
+PrintNumber(matrixResult, ElRowPos, ElColPos);
+
+int[,] GetMatrix(int rows, int columns)
+{
+  int[,] matrix = new int[rows, columns];
+  Random random = new Random();
+
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      matrix[i, j] = random.Next(1, 100);
+    }
+  }
+
+  return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      if (j == 0) Console.Write("[");
+      if (j < matrix.GetLength(1) - 1) Console.Write(matrix[i, j] + ", ");
+      else Console.Write(matrix[i, j] + "]");
+    }
+
+    Console.WriteLine();
+  }
+
+  Console.WriteLine();
+}
+
+void PrintNumber(int[,] matrix, int ElRowPos, int ElColPos)
+{
+  if (ElRowPos <= matrix.GetLength(0) & ElColPos <= matrix.GetLength(1))
+  {
+    Console.Write($"{ElRowPos}, {ElColPos} -> {matrix[ElRowPos - 1, ElColPos - 1]}");
+  }
+  else Console.Write($"{ElRowPos}, {ElColPos} -> такого числа в массиве нет");
+  
+  Console.WriteLine();
+}
---
+++ task_52/Program.cs
@@ -0,0 +1,80 @@
+﻿// Задача 52.
+// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+Console.Clear();
+Console.Write("Укажите кол-во строк матрицы: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Укажите кол-во столбцов матрицы: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+int[,] matrixResult = GetMatrix(rows, columns);
+double[] averageColArr = GetAverageColumn(matrixResult);
+
+PrintMatrix(matrixResult);
+PrintAverageArr(averageColArr);
+
+int[,] GetMatrix(int rows, int columns)
+{
+  int[,] matrix = new int[rows, columns];
+  Random random = new Random();
+
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      matrix[i, j] = random.Next(1, 10);
+    }
+  }
+
+  return matrix;
+}
+
+void PrintMatrix(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      if (j == 0) Console.Write("[");
+      if (j < array.GetLength(1) - 1) Console.Write(array[i, j] + ", ");
+      else Console.Write(array[i, j] + "]");
+    }
+
+    Console.WriteLine();
+  }
+
+  Console.WriteLine();
+}
+
+double[] GetAverageColumn(int[,] matrix)
+{
+  double[] averageColumnArr = new double[matrix.GetLength(1)];
+  
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      averageColumnArr[j] += matrix[i, j];
+      if (i == matrix.GetLength(0) - 1) averageColumnArr[j] /= matrix.GetLength(0);
+    }
+  }
+  
+  return averageColumnArr;
+}
+
+void PrintAverageArr(double[] arr)
+{
+  Console.Write("Среднее арифметическое каждого столбца: ");
+  
+  for (int i = 0; i < arr.Length; i++)
+  {
+    if (i < arr.Length - 1) Console.Write(Math.Round(arr[i], 1) + ", ");
+    else Console.Write(Math.Round(arr[i], 1) + ".");
+  }
+  
+  Console.WriteLine();
+}
